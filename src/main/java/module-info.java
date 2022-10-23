c9cb8ccf1c3672becafcf780e347aef2131fb6a1
Hicham Naoufal <7191269+hnaoufal@users.noreply.github.com>
2022-10-23 14:45:39 +0200

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.pr.gameclient {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.pr.gameclient to javafx.fxml;
+    exports com.pr.gameclient;
+}

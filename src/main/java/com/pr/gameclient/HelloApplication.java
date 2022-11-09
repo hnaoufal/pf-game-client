cb780911ef9c83587eee4b63b49c543e4772b5e1
Mandy <s64382@bht-berlin.de>
2022-11-09 22:56:53 +0100

    feat(mysql): add Person model

--- src/main/java/com/pr/gameclient/HelloApplication.java
+++ src/main/java/com/pr/gameclient/HelloApplication.java
@@ -2,22 +2,43 @@ package com.pr.gameclient;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
+
+    private Stage stage;
+    private Scene scene;
+
+    public HelloApplication() {
+        // default constructor
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+    public Scene getScene() {
+        return scene;
+    }
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Game.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Dont Get Caught!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Start.fxml"));
+            Parent root = loader.load();
+            HelloController mainWindowController = loader.getController();
+            mainWindowController.setPrimaryStage(stage);
+            Scene scene = new Scene(root);
+            stage.setTitle("");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/com/pr/gameclient/HelloController.java
+++ src/main/java/com/pr/gameclient/HelloController.java
@@ -1,14 +1,41 @@
 package com.pr.gameclient;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
+    private FXMLLoader fxmlLoader;
+    private Parent root;
+
+    public void setPrimaryStage(Stage stage) {
+        this.stage = stage;
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onButtonAnleitung() {
+        try {
+            scene = stage.getScene();
+            fxmlLoader = new FXMLLoader(getClass().getResource("Instructions.fxml"));
+            root = fxmlLoader.load();
+            scene.setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onButtonLogin() {
+
+    }
+
+    public void onButtonBeenden() {
+        System.exit(0);
     }
 }

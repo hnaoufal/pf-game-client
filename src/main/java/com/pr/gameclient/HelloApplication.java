babd0714fb70f063cc431a75616d6d45ca3d04f3
Mandy <s64382@bht-berlin.de>
2022-11-10 13:06:46 +0100

    feat(mysql): add Person model

--- src/main/java/com/pr/gameclient/HelloApplication.java
+++ src/main/java/com/pr/gameclient/HelloApplication.java
@@ -9,36 +9,26 @@ import javafx.stage.Stage;
 public class HelloApplication extends Application {
 
     private Stage stage;
-    private Scene scene;
 
-    public HelloApplication() {
-        // default constructor
-    }
+    public static void main(String[] args) {
 
-    public Stage getStage() {
-        return stage;
-    }
-    public Scene getScene() {
-        return scene;
+        launch(args);
     }
 
     @Override
-    public void start(Stage stage) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("Start.fxml"));
-            Parent root = loader.load();
-            HelloController mainWindowController = loader.getController();
-            mainWindowController.setPrimaryStage(stage);
-            Scene scene = new Scene(root);
-            stage.setTitle("");
-            stage.setScene(scene);
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    public void start(Stage stage)  throws Exception {
 
-    public static void main(String[] args) {
-        launch(args);
+        this.stage = stage;
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Start.fxml"));
+
+        Parent root = loader.load();
+        HelloController startWindowController = loader.getController();
+        startWindowController.setStage(stage);
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Dont get Caught");
+        stage.show();
     }
 }
--- src/main/java/com/pr/gameclient/HelloController.java
+++ src/main/java/com/pr/gameclient/HelloController.java
@@ -6,36 +6,81 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloController {
+
     @FXML
-    private Stage stage;
-    private Scene scene;
     private FXMLLoader fxmlLoader;
     private Parent root;
+    private Scene sceneSwitcher;
+    private Stage stage;
 
-    public void setPrimaryStage(Stage stage) {
+    public void setStage(Stage stage) {
         this.stage = stage;
     }
 
+    // Zur Beschreibung
     @FXML
-    protected void onButtonAnleitung() {
-        try {
-            scene = stage.getScene();
-            fxmlLoader = new FXMLLoader(getClass().getResource("Instructions.fxml"));
-            root = fxmlLoader.load();
-            scene.setRoot(root);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    protected void onButtonBeschreibung() throws Exception {
+
+        fxmlLoader = new FXMLLoader(getClass().getResource("Description.fxml"));
+        root = fxmlLoader.load();
+        sceneSwitcher = stage.getScene();
+        sceneSwitcher.setRoot(root);
     }
 
-    public void onButtonLogin() {
+    // Zum Login
+    @FXML
+    public void onButtonZumLogin() throws Exception {
 
+        fxmlLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
+        root = fxmlLoader.load();
+        sceneSwitcher = stage.getScene();
+        sceneSwitcher.setRoot(root);
     }
 
-    public void onButtonBeenden() {
+    // Zur Registrierung
+    @FXML
+    public void onButtonZurRegistrierung() throws Exception {
+
+        fxmlLoader = new FXMLLoader(getClass().getResource("Registry.fxml"));
+        root = fxmlLoader.load();
+        sceneSwitcher = stage.getScene();
+        sceneSwitcher.setRoot(root);
+    }
+
+    // Zurück zum Startfenster
+    @FXML
+    protected void onButtonStart() throws Exception {
+
+        fxmlLoader = new FXMLLoader(getClass().getResource("Start.fxml"));
+        root = fxmlLoader.load();
+        sceneSwitcher = stage.getScene();
+        sceneSwitcher.setRoot(root);
+    }
+
+    // Login und zum Fenster "Game" springen
+    @FXML
+    public void onButtonLogin() throws Exception {
+
+        fxmlLoader = new FXMLLoader(getClass().getResource("Game.fxml"));
+        root = fxmlLoader.load();
+        sceneSwitcher = stage.getScene();
+        sceneSwitcher.setRoot(root);
+    }
+
+    // Registrierung und zum Fenster "Game" springen
+    @FXML
+    public void onButtonRegistrierung() throws Exception {
+
+        fxmlLoader = new FXMLLoader(getClass().getResource("Game.fxml"));
+        root = fxmlLoader.load();
+        sceneSwitcher = stage.getScene();
+        sceneSwitcher.setRoot(root);
+    }
+
+    // Spiel beenden
+    @FXML
+    public void onButtonBeenden() throws Exception {
         System.exit(0);
     }
 }
